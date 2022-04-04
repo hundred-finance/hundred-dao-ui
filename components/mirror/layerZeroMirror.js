6ec59922ddbdd481fcdc534a6375532a7fcb1b48
SHA-2084 <2048.sha@gmail.com>
2022-04-04 20:34:59 +0200

    hide mirroring option when not supported

--- components/mirror/layerZeroMirror.js
+++ components/mirror/layerZeroMirror.js
@@ -25,53 +25,64 @@ export default function LayerZeroMirror({ project }) {
       <Typography variant="h5" className={classes.sectionsubHeader}>
         powered by LayerZero
       </Typography>
-      <div className={classes.overviewCard}>
-        <div className={classes.inputTitleContainer}>
-          <div className={classes.inputTitle}>
-            <Typography variant="h5" noWrap>
-              Select supported chain
-            </Typography>
-          </div>
-        </div>
-        <Autocomplete
-          disableClearable={true}
-          options={project.targetChainMirrorGates.filter((t) => t.hasActiveMveHND)}
-          value={targetChain}
-          onChange={onTargetSelectChanged}
-          getOptionLabel={(option) => option.name}
-          fullWidth={true}
-          renderOption={(option, { selected }) => (
-            <React.Fragment>
-              <div className={classes.text}>{option.name}</div>
-            </React.Fragment>
-          )}
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              InputProps={{
-                ...params.InputProps,
-                ...{
-                  placeholder: 'Search target chain',
-                },
-              }}
-              variant="outlined"
+      {project.layerZero ? (
+        <>
+          <div className={classes.overviewCard}>
+            <div className={classes.inputTitleContainer}>
+              <div className={classes.inputTitle}>
+                <Typography variant="h5" noWrap>
+                  Select supported chain
+                </Typography>
+              </div>
+            </div>
+            <Autocomplete
+              disableClearable={true}
+              options={project.targetChainMirrorGates.filter((t) => t.hasActiveMveHND)}
+              value={targetChain}
+              onChange={onTargetSelectChanged}
+              getOptionLabel={(option) => option.name}
+              fullWidth={true}
+              renderOption={(option, { selected }) => (
+                <React.Fragment>
+                  <div className={classes.text}>{option.name}</div>
+                </React.Fragment>
+              )}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  InputProps={{
+                    ...params.InputProps,
+                    ...{
+                      placeholder: 'Search target chain',
+                    },
+                  }}
+                  variant="outlined"
+                />
+              )}
             />
-          )}
-        />
-      </div>
-      <div className={classes.overviewCard}>
-        <Button
-          fullWidth
-          disableElevation
-          variant="contained"
-          color="primary"
-          size="large"
-          onClick={onMirror}
-          disabled={targetChain === undefined || +project?.veTokenMetadata?.localBalance === 0}
-        >
-          Mirror {formatCurrency(project.veTokenMetadata.localBalance)} veHND
-        </Button>
-      </div>
+          </div>
+          <div className={classes.overviewCard}>
+            <Button
+              fullWidth
+              disableElevation
+              variant="contained"
+              color="primary"
+              size="large"
+              onClick={onMirror}
+              disabled={targetChain === undefined || +project?.veTokenMetadata?.localBalance === 0}
+            >
+              Mirror {formatCurrency(project.veTokenMetadata.localBalance)} veHND
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div className={classes.overviewCard}>
+          <Typography variant="h3" className={classes.subsectionHeader}>
+            Mirroring not supported yet from {project.name}
+          </Typography>
+        </div>
+      )}
+
       {project?.mirrored_locks?.length > 0 ? (
         <div className={classes.overviewCard}>
           <Typography variant="h3" className={classes.subsectionHeader}>
