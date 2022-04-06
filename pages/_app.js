c047e367bbd177a8ec2f2243f1fc1063ae964b81
nipa85 <nikos.pap85@gmail.com>
2022-04-06 12:43:30 +0300

    change page title

--- pages/_app.js
+++ pages/_app.js
@@ -60,7 +60,7 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <React.Fragment>
       <Head>
-        <title>Hundred Finance DAO</title>
+        <title>Vote - Hundred Finance</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={themeConfig}>
